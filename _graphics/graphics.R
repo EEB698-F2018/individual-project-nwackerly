86752a9f31b5616211a1351ad1bed309a7f87868
Your Name <you@example.com>
2018-11-21 11:42:06 -0500

    spacing, nothing new really

--- _graphics/graphics.R
+++ _graphics/graphics.R
@@ -77,4 +77,3 @@ ggplot(prelim_temp, aes(sun))+
 m1 <- lmer(therm_t ~ pos_beh + time_od + sun + date + hab_type + 
        (1|individual), data = prelim_temp)
 prelim_temp$pred <- predict(m1, type="response")
-
