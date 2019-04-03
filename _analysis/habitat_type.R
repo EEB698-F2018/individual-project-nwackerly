38c4a9a70173b276d320155fa06ca5f757f8c6cc
Nicolette Wackerly <nicolettewackerly@Nicolettes-MacBook-Pro.local>
2019-04-03 11:13:28 -0500

    changed libraries loaded

--- _analysis/habitat_type.R
+++ _analysis/habitat_type.R
@@ -1,11 +1,8 @@
 # load libraries
-library(tidyverse) 
-library(readr) 
+library(tidyverse)
+library(readr)
 library(lubridate)
 library(magrittr)
-library(ggplot2)
-library(car)
-library(emmeans)
 library(lme4)
 
 #read in data
